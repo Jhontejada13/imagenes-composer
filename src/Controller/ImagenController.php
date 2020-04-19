f2bfa5040f8b36b7bde66d80a3e599d645bb2f2f
Jhon Tejada <jhontejada13@gmail.com>
2020-04-19 18:09:29 -0500

    Se agregan métodos CRUD desde controller

--- src/Controller/ImagenController.php
+++ src/Controller/ImagenController.php
@@ -14,8 +14,27 @@ class ImagenController extends AbstractController
      */
     public function index()
     {
+        $imagen_repo = $this->getDoctrine()->getRepository(Imagen::class);
+
+        $imagenes = $imagen_repo->findAll();
+
+        // Me saca un valor de la base de datos según la condición, aunque existan varios sólo trae uno
+        // $imagen = $imagen_repo->findOneBy([
+        //     'nombre' => 'imagen2' //Condición
+        // ]);
+
+
+        // Me saca los valores que coincidan con la condición, todos los que haya en la base de datos
+        // $imagen = $imagen_repo->findBy([
+        //     'nombre' => 'imagen2'
+        // ], [
+        //     'id' => 'DESC' //Ordena la entrega de los registros en pantalla de manera descendente
+        // ]);
+        
+
         return $this->render('imagen/index.html.twig', [
             'controller_name' => 'ImagenController',
+            'imagenes' => $imagenes
         ]);
     }
 
@@ -25,7 +44,7 @@ class ImagenController extends AbstractController
 
         //Creo objeto y le doy valores
         $imagen = new Imagen();
-        $imagen->setNombre('imagen1');
+        $imagen->setNombre('imagen5');
         $imagen->setDescripcion('imagen de prueba, a ver qué pasa');
 
         //Persiste el objeto en doctrine
@@ -36,4 +55,76 @@ class ImagenController extends AbstractController
 
         return new Response('Se guardo la imagen con el id '.$imagen->getId());
     }
+
+    public function imagen(Imagen $imagen){
+        // //Buscar en base de datos
+        // //Cargar un repositorio
+        // $imagen_repo = $this->getDoctrine()->getRepository(Imagen::class);
+
+        // //Consulta
+        // $imagen = $imagen_repo->find($id);
+
+        //Comprobar resultado
+        if (!$imagen) {
+            $message = 'La imagen no existe';
+        }else{
+            $message = 'Has elegido la imagen: '.$imagen->getNombre().'-'.$imagen->getDescripcion();
+        }
+
+        return new Response($message);
+
+    }
+
+    public function update($id){
+        //Cargar doctrine
+        $doctrine = $this->getDoctrine();
+
+        //Cargar entityManager
+        $entityManager = $doctrine->getManager();
+
+        //Cargar repo
+        $imagen_repo = $entityManager->getRepository(Imagen::class);
+
+        //Find para conseguir el objeto
+        $imagen = $imagen_repo->find($id);
+
+        //Comprobar si llega el objeto
+        if(!$imagen){
+            $message = 'La imagen no existe en la BD';
+        }else{
+            //Asignar valores a objeto conseguido
+            $imagen->setNombre('Nombre cambiado');
+            $imagen->setDescripcion('Descripcion cambiada');
+
+            //Persistir el objeto 
+            $entityManager->persist($imagen);
+
+            //Guardar en BD
+            $entityManager->flush();
+            $message = 'Se actualizó el registro con éxito'.$imagen->getID();
+        }
+
+        //Responde
+        return new Response($message);
+
+    }
+
+    public function delete(Imagen $imagen){
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if ($imagen && is_object($imagen)) {
+            $entityManager->remove($imagen);
+            $entityManager->flush();
+
+            $mesaage = 'Imagen borrado correctamente';
+        }else{
+            $message = 'No se encuentra la imagen';
+        }
+
+        return new Response($mesaage);
+
+    }
+
+
 }
